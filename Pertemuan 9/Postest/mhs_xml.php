7f0a25ff72fde2a020c12e03e2f5aecb0e902eb0
Alfian Hakim <79136106+alfiankidz@users.noreply.github.com>
2021-12-19 11:10:41 +0700

    Update mhs_xml.php

--- Pertemuan 9/Postest/mhs_xml.php
+++ Pertemuan 9/Postest/mhs_xml.php
@@ -1,18 +1,18 @@
 <?php
-include "koneksi.php";
-header('Content-Type: text/xml');
-$query = "SELECT * FROM mahasiswa";
-$hasil = mysqli_query($con, $query);
-$jumField = mysqli_num_fields($hasil);
-echo "<?xml version='1.0'?>";
-echo "<data>";
-while ($data = mysqli_fetch_array($hasil)) {
-    echo "<mahasiswa>";
-    echo "<nim>", $data['nim'], "</nim>";
-    echo "<nama>", $data['nama'], "</nama>";
-    echo "<jkel>", $data['jkel'], "</jkel>";
-    echo "<alamat>", $data['alamat'], "</alamat>";
-    echo "<tgllhr>", $data['tgllhr'], "</tgllhr>";
-    echo "</mahasiswa>";
+include "koneksi.php"; // Menyertakan koneksi ke database
+header('Content-Type: text/xml'); // Memberikan header pengenal bahwa program tersebut adalah skrip XML
+$query = "SELECT * FROM mahasiswa"; // SQL Query mengambil data pada tabel mahasiswa
+$hasil = mysqli_query($con, $query); // Eksekusi SQL Query
+$jumField = mysqli_num_fields($hasil); // Mengambil berapa jumlah field
+echo "<?xml version='1.0'?>"; // Pengenal Skrip XML ke dalam versi penulisan PHP
+echo "<data>"; // Pembuka tag data
+while ($data = mysqli_fetch_array($hasil)) { // Fetching data database sampai habis
+    echo "<mahasiswa>"; // Pembuka tag mahasiswa 
+    echo "<nim>", $data['nim'], "</nim>"; // Menampilkan nim
+    echo "<nama>", $data['nama'], "</nama>"; // Menampilkan nama
+    echo "<jkel>", $data['jkel'], "</jkel>"; // Menampilkan jkel
+    echo "<alamat>", $data['alamat'], "</alamat>"; // Menampilkan alamat
+    echo "<tgllhr>", $data['tgllhr'], "</tgllhr>"; // Menampilkan tgllhr
+    echo "</mahasiswa>"; // Penutup tag mahasiswa
 }
-echo "</data>";
+echo "</data>"; // Penutup tag data
